9c7713a8e51779d28553aae72c4af69f3e553036
jatin-js <you@example.co  git config --global user.name Your>
2020-01-17 11:21:12 +0530

    PHP include and require

---
+++ website1/contact.php
@@ -0,0 +1,6 @@
+<?php include 'inc/header.php';?>
+    <h1>Contact</h1>
+<?php include 'inc/footer.php';?>
+
+
+ <?php //include('inc/abc.php');    This is also correct(function format)?>
---
+++ website1/inc/footer.php
@@ -0,0 +1,6 @@
+
+    <footer>
+        <p>My website &copy; 2020</p>
+    </footer>
+</body>
+</html>
