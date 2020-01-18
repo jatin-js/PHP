1c4527a8f195fe8d972430f52349d21e142f709b
jatin-js <you@example.co  git config --global user.name Your>
2020-01-18 22:45:48 +0530

    PHP filters

---
+++ filters.php
@@ -0,0 +1,106 @@
+<?php
+
+# check for posted data
+// if (filter_has_var(INPUT_POST, 'data')) {
+//     echo 'Data found';
+// } else {
+//     echo 'No data';
+// }
+
+// if (filter_has_var(INPUT_POST, 'data')) {
+//     $email = $_POST['data'];
+
+// if (filter_input(INPUT_POST, 'data', FILTER_VALIDATE_EMAIL)) {
+//     echo 'Email is valid';
+// } else {
+//     echo 'Email is not valid';
+// }
+
+# Remove illegal characters
+//     $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+//     echo $email . '<br>';
+
+//     if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+//         echo 'Email is valid';
+//     } else {
+//         echo 'Email is not valid';
+//     }
+// }
+
+# FILTER_VALIDATE_BOOLEAN
+# FILTER_VALIDATE_EMAIL
+# FILTER_VALIDATE_FLOAT
+# FILTER_VALIDATE_INT
+# FILTER_VALIDATE_IP
+# FILTER_VALIDATE_REGEXP
+# FILTER_VALIDATE_URL
+
+# FILTER_SANITIZE_EMAIL
+# FILTER_SANITIZE_ENCODED
+# FILTER_SANITIZE_NUMBER_FLOAT
+# FILTER_SANITIZE_NUMBER_INT
+# FILTER_SANITIZE_SPECIAL_CHARS
+# FILTER_SANITIZE_STRING
+# FILTER_SANITIZE_URL
+
+// $var = '23dsf';
+
+// if (filter_var($var, FILTER_VALIDATE_INT)) {
+//     echo $var . ' is a number.';
+// } else {
+//     echo $var . ' is not number.';
+// }
+
+// $var = '33dfdf34234dfsdf';
+// var_dump(filter_var($var, FILTER_SANITIZE_NUMBER_INT));
+
+// $var = '<script>alert(1)</script>';
+// echo filter_var($var, FILTER_SANITIZE_SPECIAL_CHARS);
+
+# Multiple inputs
+// $filters = array(
+//     "data" => FILTER_VALIDATE_EMAIL,
+//     "data2" => array(
+//         "filter" => FILTER_VALIDATE_INT,
+//         "options" => array(
+//             "min_range" => 1,
+//             "max_range" => 100,
+//         ),
+
+//     ),
+// );
+
+// print_r(filter_input_array(INPUT_POST, $filters));
+
+$arr = array(
+    "name" => "jatin singh",
+    "age" => "133",
+    "email" => "jatin@gmail..com",
+);
+
+$filters = array(
+    "name" => array(
+        "filter" => FILTER_CALLBACK,
+        "options" => "ucwords",
+    ),
+    "age" => array(
+        "filter" => FILTER_VALIDATE_INT,
+        "options" => array(
+            "min_range" => 1,
+            "max_range" => 120,
+        ),
+    ),
+    "email" => FILTER_VALIDATE_EMAIL,
+
+);
+
+print_r(filter_var_array($arr, $filters));
+
+?>
+
+
+<form method='POST' action=<?php echo $_SERVER['PHP_SELF'] ?>>
+    <input type="text" name='data'>
+    <input type="text" name='data2'>
+    <button type='submit'>Submit</button>
+</form>
